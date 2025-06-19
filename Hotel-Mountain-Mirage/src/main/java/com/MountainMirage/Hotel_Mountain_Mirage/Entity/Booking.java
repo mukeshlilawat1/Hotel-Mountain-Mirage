04b494d517334aee3cee7e08e885c92dab2d018b
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-06-19 12:43:20 +0530

    Implemented initial structure for Booking functionality

--- Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Entity/Booking.java
+++ Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Entity/Booking.java
@@ -1,12 +1,18 @@
 package com.MountainMirage.Hotel_Mountain_Mirage.Entity;
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Data
 @Entity
+@Getter
+@Setter
 @Table(name = "booking")
 public class Booking {
 
@@ -14,18 +20,60 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookingId;
 
+    @NotNull(message = "check in date is required")
     private LocalDate checkInDate;
+
+    @Future(message = "check out date must be in the future")
     private  LocalDate checkOutDate;
+
+    @Min(value = 1, message = "number of adults should not be less than one")
     private int numOfAdults;
+
+    @Min(value = 0, message = "number of children should not be less than zero")
     private  int numOfChildren;
     private int totalNumOfGuest;
 
     private String bookingConfirmationCode;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "Room_Id")
+    @JoinColumn(name = "room_id")
     private Room room;
-    
+
+    public void calculateTotalNumberOfGuests() {
+        this.totalNumOfGuest = this.numOfAdults + this.numOfChildren;
+    }
+
+//    public int getNumOfAdults() {
+//        return numOfAdults;
+//    }
+//
+//    public int getNumOfChildren() {
+//        return numOfChildren;
+//    }
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumberOfGuests();
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumberOfGuests();
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "bookingId=" + bookingId +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                ", numOfAdults=" + numOfAdults +
+                ", numOfChildren=" + numOfChildren +
+                ", totalNumOfGuest=" + totalNumOfGuest +
+                ", bookingConfirmationCode='" + bookingConfirmationCode + '\'' +
+                '}';
+    }
 }
