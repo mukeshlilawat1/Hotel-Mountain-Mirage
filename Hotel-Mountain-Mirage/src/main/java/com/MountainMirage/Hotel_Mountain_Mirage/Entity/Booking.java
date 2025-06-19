ef5e58c933249e41438942e070be2f3997937bc5
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-06-19 22:13:09 +0530

    Implemented initial structure for DTO functionality

--- Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Entity/Booking.java
+++ Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Entity/Booking.java
@@ -35,7 +35,7 @@ public class Booking {
 
     private String bookingConfirmationCode;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User user;
 
