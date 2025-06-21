6121468363e7b43a477a6ada1e6fb8c210cbbaa6
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-06-21 15:13:35 +0530

    utils file add

---
+++ Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Utils/Utils.java
@@ -0,0 +1,22 @@
+package com.MountainMirage.Hotel_Mountain_Mirage.Utils;
+
+import java.security.SecureRandom;
+
+public class Utils {
+
+    private static final String ALPHANUMERIC_STRING = "ABCDEFGHIJKLMONPQRSTUVWXYZ0123456789";
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    public static String generateRandomConfirmationCode(int length) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i <length; i++) {
+            int randomIndex = secureRandom.nextInt(ALPHANUMERIC_STRING.length());
+            char randomChar = ALPHANUMERIC_STRING.charAt(randomIndex);
+            stringBuilder.append(randomChar);
+        }
+        return stringBuilder.toString();
+    }
+    
+
+}
