966e8eab0b9774b52ec4e636b0ef956aae415701
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-06-25 22:37:57 +0530

    Work on RoomService

--- Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Service/Imp/RoomService.java
+++ Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Service/Imp/RoomService.java
@@ -1,11 +1,16 @@
 package com.MountainMirage.Hotel_Mountain_Mirage.Service.Imp;
 
 import com.MountainMirage.Hotel_Mountain_Mirage.Dto.Response;
+import com.MountainMirage.Hotel_Mountain_Mirage.Dto.RoomDTO;
+import com.MountainMirage.Hotel_Mountain_Mirage.Entity.Room;
+import com.MountainMirage.Hotel_Mountain_Mirage.Exception.OurException;
 import com.MountainMirage.Hotel_Mountain_Mirage.Repo.BookingRepository;
 import com.MountainMirage.Hotel_Mountain_Mirage.Repo.RoomRepository;
 import com.MountainMirage.Hotel_Mountain_Mirage.Service.AWSS3Service;
 import com.MountainMirage.Hotel_Mountain_Mirage.Service.Interfac.IRoomService;
+import com.MountainMirage.Hotel_Mountain_Mirage.Utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,29 +30,77 @@ public class RoomService implements IRoomService {
     @Autowired
     private AWSS3Service awss3Service;
 
-
     @Override
     public Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice, String description) {
-        return null;
+        Response response = new Response();
+
+        try {
+            String imageUrl = awss3Service.saveImageToS3(photo);
+            Room room = new Room();
+            room.setRoomPhotoUrl(imageUrl);
+            room.setRoomType(roomType);
+            room.setRoomPrice(roomPrice);
+            room.setDescription(description);
+
+            Room savedRoom = roomRepository.save(room);
+            RoomDTO roomDTO = Utils.mapRoomEntityToRoomDTO(savedRoom);
+
+            response.setRoom(roomDTO);
+            response.setMessage("Room Added Successfully");
+            response.setStatusCode(200);
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Getting a User Info " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public List<String> getAllRoomTypes() {
-        return List.of();
+        return roomRepository.findDistinctRoomTypes();
     }
 
     @Override
     public Response getAllRooms() {
-        return null;
+        Response response = new Response();
+
+        try {
+            List<Room> roomList = roomRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+            List<RoomDTO> roomDTOList = Utils.mapRoomListEntityToUserListDTO(roomList);
+
+            response.setMessage("Successfully");
+            response.setStatusCode(200);
+            response.setRoomList(roomDTOList);
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error getting all rooms" + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response deleteRoom(Long roomId) {
-        return null;
+        Response response = new Response();
+
+        try {
+            roomRepository.findById(roomId).orElseThrow(() -> new OurException("Room Not Found"));
+            roomRepository.deleteById(roomId);
+            response.setMessage("Successfully");
+            response.setStatusCode(200);
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Deleting a Room " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
-    public Response updateRoom(Long roomId, String description, String roomType, BigDecimal roomPrice, MultipartFile photo) {
+    public Response updateRoom(Long roomId, String description, String roomType, BigDecimal roomPrice,
+            MultipartFile photo) {
         return null;
     }
 
