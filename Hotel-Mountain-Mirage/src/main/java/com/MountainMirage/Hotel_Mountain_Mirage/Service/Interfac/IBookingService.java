91a457588268fd9e9fc5b0b26ae02a0d137fa1d1
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-09-23 10:16:24 +0530

    Added booking availability check feature

--- Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Service/Interfac/IBookingService.java
+++ Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Service/Interfac/IBookingService.java
@@ -1,5 +1,11 @@
 package com.MountainMirage.Hotel_Mountain_Mirage.Service.Interfac;
 
-public class IBookingService {
+import com.MountainMirage.Hotel_Mountain_Mirage.Dto.Response;
+import com.MountainMirage.Hotel_Mountain_Mirage.Entity.Booking;
 
+public interface IBookingService  {
+ Response saveBooking(Long rootId, Long userId, Booking bookingRequest);
+      Response findBookingByConfirmationCode(String confirmationCode);
+      Response getAllBookings();
+      Response cancelBooking();
 }
