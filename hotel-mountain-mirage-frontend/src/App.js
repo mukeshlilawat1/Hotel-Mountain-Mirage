2898cdb17410c6ef9c87d2d41bbc7ed426bc29f2
mukeshlilawat <mukeshkumarlilawat1@gmail.com>
2025-11-05 00:27:47 +0530

    ✨ Revamp: Fully redesigned AllRoomsPage with premium modern UI using TailwindCSS

--- hotel-mountain-mirage-frontend/src/App.js
+++ hotel-mountain-mirage-frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './component/common/Navbar';
 import HomePage from './component/home/Home';
 import FooterComponent from './component/common/Footer';
+import AllRoomsPage from './component/booking_rooms/AllRoomsPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <div className='content'>
           <Routes>
             <Route exact path='/home' element={<HomePage />} />
+            <Route exact path='/rooms' element={<AllRoomsPage />} />
           </Routes>
 
         </div>
