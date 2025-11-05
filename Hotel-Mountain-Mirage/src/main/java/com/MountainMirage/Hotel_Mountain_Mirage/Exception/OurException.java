05ecc58809f4f75cde3f20369c9d2e345a83b864
Mukesh Lilawat <143742837+mukeshlilawat1@users.noreply.github.com>
2025-11-05 12:56:19 +0530

    Delete Hotel-Mountain-Mirage directory

--- Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Exception/OurException.java
+++
@@ -1,8 +0,0 @@
-package com.MountainMirage.Hotel_Mountain_Mirage.Exception;
-
-public class OurException extends RuntimeException{
-
-    public OurException(String message) {
-        super(message);
-    }
-}
