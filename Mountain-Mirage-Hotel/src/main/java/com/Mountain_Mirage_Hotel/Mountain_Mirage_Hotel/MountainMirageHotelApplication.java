e2a941aeb6541ea8e3fe99976e6e2ae70abdf885
mukeshlilawat <mukeshkumarlilawat1@gmail.com>
2025-11-17 11:07:13 +0530

    backend part uploaded

---
+++ Mountain-Mirage-Hotel/src/main/java/com/Mountain_Mirage_Hotel/Mountain_Mirage_Hotel/MountainMirageHotelApplication.java
@@ -0,0 +1,13 @@
+package com.Mountain_Mirage_Hotel.Mountain_Mirage_Hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainMirageHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainMirageHotelApplication.class, args);
+	}
+
+}
