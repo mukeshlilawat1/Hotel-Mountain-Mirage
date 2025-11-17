3c2c589c851d792f07ece378ea568cdc3d5a02d6
mukeshlilawat <mukeshkumarlilawat1@gmail.com>
2025-11-17 11:07:13 +0530

    backend part uploaded

--- Hotel-Mountain-Mirage/src/main/java/com/MountainMirage/Hotel_Mountain_Mirage/Utils/config/AdminSeeder.java
+++
@@ -1,4 +0,0 @@
-package com.MountainMirage.Hotel_Mountain_Mirage.Utils.config;
-
-public class AdminSeeder {
-}
---
+++ Mountain-Mirage-Hotel/src/test/java/com/Mountain_Mirage_Hotel/Mountain_Mirage_Hotel/MountainMirageHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mountain_Mirage_Hotel.Mountain_Mirage_Hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainMirageHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
