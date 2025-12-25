0f43012e727a82e15824c57c359a52632ffe618b
Mukesh Lilawat <mukeshkumarlilawat1@gmail.com>
2025-12-25 23:03:38 +0530

    feat: initialize hotel management backend structure
    - Added DTOs for user, room, booking, and authentication
    - Defined core entities for User, Room, and Booking
    - Organized layered architecture (controller, service, repository, security)
    - Prepared base response wrapper for API consistency

---
+++ Mountain-Mirage-Hotel/src/main/java/com/Mountain_Mirage_Hotel/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package com.Mountain_Mirage_Hotel.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+
+    private Long id;
+    private String email;
+    private String name;
+    private String phonenumber;
+    private String role;
+    private List<BookingDTO> bookings = new ArrayList<>();
+
+
+}
---
+++ Mountain-Mirage-Hotel/src/main/java/com/Mountain_Mirage_Hotel/entity/Room.java
@@ -0,0 +1,41 @@
+package com.Mountain_Mirage_Hotel.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@ToString
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<Booking> bookings = new ArrayList<>();
+
+
+//    @Override
+//    public String toString() {
+//        return "Room{" +
+//                "id=" + id +
+//                ", roomType='" + roomType + '\'' +
+//                ", roomPrice=" + roomPrice +
+//                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+//                ", description='" + roomDescription + '\'' +
+//                '}';
+//    }
+}
